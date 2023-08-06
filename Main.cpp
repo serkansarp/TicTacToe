8866d8660a2b4ae777c7bb5275c73b7a855e4111
serkansarp <serkansarp79@gmail.com>
2023-08-06 14:47:00 +0300

    changes

---
+++ Main.cpp
@@ -0,0 +1,12 @@
+﻿// TicTacToe.cpp : Defines the entry point for the application.
+//
+
+#include "TicTacToe.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
